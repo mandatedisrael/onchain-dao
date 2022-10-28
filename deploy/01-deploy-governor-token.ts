b75f1371f9e354f2cdbf2f21c360f2a33d3dec4c
mandatedisrael <mandatedisrael@gmail.com>
2022-10-28 12:17:38 +0100

    spunning hardhat local blockchain

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -3,4 +3,17 @@ import { DeployFunction } from "hardhat-deploy/types"
 
 const deployGovernanceToken: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
-    ) {};
+    ) {
+        const {getNamedAccounts, deployments, network} = hre;
+        const { deploy, log } = deployments;
+        const { deployer } = await getNamedAccounts();
+        log("Deploying Governance Token...");
+        const governanceToken = await deploy("GovernanceToken", {
+            from: deployer,
+            args: [],
+            log: true,
+        }); 
+        log(`GovernorToken deployed to ${governanceToken.address}`);
+    };
+
+    export default deployGovernanceToken;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,28 @@
 import "hardhat-deploy"
 import "@nomiclabs/hardhat-ethers"
 import "@typechain/hardhat"
-
+import { HardhatUserConfig } from "hardhat/config"
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.17",
-};
+  networks: {
+    hardhat: {
+      chainId:31337
+    },
+    localhost: {
+      chainId:31337
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+}
+
+export default config;
