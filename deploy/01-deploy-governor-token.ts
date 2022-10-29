4f2197fe483628a0d6201600b6512365942f78c6
mandatedisrael <mandatedisrael@gmail.com>
2022-10-29 07:58:06 +0100

    Created a delegated function

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,5 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { ethers } from "hardhat";
 
 const deployGovernanceToken: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
@@ -14,6 +15,19 @@ const deployGovernanceToken: DeployFunction = async function (
             log: true,
         }); 
         log(`GovernorToken deployed to ${governanceToken.address}`);
+        await delegate(governanceToken.address, deployer);
+        log(`Delegated ${deployer} to ${governanceToken.address}`);
+         
     };
+
+    //function to delegate governance token to a deployer so they have the ability to vote ~ Hey, take my vote, you can use my vote~
+    const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+        // get a deployed instance of the governance token
+        const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress); 
+        const tx = await governanceToken.delegate(delegatedAccount);
+        await tx.wait(1); //wait for transaction to be mined by 1 block
+        console.log(`Checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`);
+    }
+    
 
     export default deployGovernanceToken;
