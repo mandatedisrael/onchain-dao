ad665ec1f0df6ab9bd1741c2f9f716800db4f498
mandatedisrael <mandatedisrael@gmail.com>
2022-10-29 22:52:41 +0100

    Created deploy function for GovernorContract but not finished

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,1 +1,4 @@
 export const MIN_DELAY = 3600 ; // 1 hour in seconds
+export const VOTING_PERIOD = 5; // 5 BLOCKS
+export const VOTING_DELAY = 1; // 1 BLOCK
+export const QUORUM_PERCENTAGE = 4; // 4% PERCENTAGE OF VOTERS atleast for a vote to pass
