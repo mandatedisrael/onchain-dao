8cc993abea79036425260e04dd65d4d99a813aff
mandatedisrael <mandatedisrael@gmail.com>
2022-10-31 21:45:30 +0100

    scripts needed for interfacing

---
+++ scripts/propose.ts
@@ -0,0 +1,7 @@
+import { ethers } from "hardhat";
+
+export async function propose() {
+    const governor = await ethers.getContract("GovernorContract");
+    const box = await ethers.getContract("Box");
+    // working on it..
+}
